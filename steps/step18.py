a09c823a23f22982eda445ccb510fb1defa62232
김도현 <kkdh15@dohyunki-MacBookAir.local>
2025-04-06 18:22:24 +0900

    step18 메모리 절약

---
+++ steps/step18.py
@@ -0,0 +1,143 @@
+
+
+import weakref
+import numpy as np
+
+class Variable:
+    def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError('{}은(는) 지원하지 않습니다.'.format(type(data)))
+    
+        self.data = data
+        self.grad = None
+        self.creator = None
+        self.generation = 0 # 세대 수 기록하는 변수
+    
+    def set_creator(self, func):
+        self.creator = func
+        self.generation = func.generation + 1
+    
+    def backward(self, retain_grad=False): # 중간 변수의 미분값 저장 안 함
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+        
+        funcs = []
+        seen_set = set()
+        
+        def add_func(f): # 
+            if f not in seen_set:
+                funcs.append(f)
+                seen_set.add(f)
+                funcs.sort(key=lambda x: x.generation)
+        
+        add_func(self.creator)
+        
+        while funcs:
+            f = funcs.pop()
+            gys = [output().grad for output in f.outputs]
+            gxs = f.backward(*gys)
+            if not isinstance(gxs, tuple):
+                gxs = (gxs,)
+            
+            for x, gx in zip(f.inputs, gxs):
+                if x.grad is None:
+                    x.grad = gx
+                else:
+                    x.grad = x.grad + gx
+                
+                if x.creator is not None:
+                    add_func(x.creator)
+                
+            if not retain_grad:
+                for y in f.outputs:
+                    y().grad = None #약한 참조
+    
+    def cleargrad(self):
+        self.grad = None
+
+class Config:
+    enable_backprop = True
+
+class Function:
+    def __call__(self, *inputs):
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs)
+        if not isinstance(ys, tuple):
+            ys = (ys,)
+        outputs = [Variable(as_array(y)) for y in ys]
+        
+        if Config.enable_backprop:
+            self.generation = max([x.generation for x in  inputs]) # 세대 설정
+            for output in outputs:
+                output.set_creator(self) # 연결 설정
+            self.inputs = inputs
+            self.outputs = [weakref.ref(output) for output in outputs]
+        
+        return outputs if len(outputs) > 1 else outputs[0]
+    
+    def forward(self, xs):
+        raise NotImplementedError()
+    
+    def backward(self, gys):
+        raise NotImplementedError()
+    
+class Square(Function):
+    def forward(self, x):
+        y = x ** 2
+        return y
+    
+    def backward(self, gy):
+        x = self.inputs[0].data
+        gx = 2 * x * gy
+        return gx
+
+class Exp(Function):
+    def forward(self, x):
+        y = np.exp(x)
+        return y
+    
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
+    
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+def square(x):
+    return Square()(x)
+
+def exp(x):
+    return Exp()(x)
+
+class Add(Function):
+    def forward(self, x0, x1):
+        y = x0 + x1
+        return y
+    
+    def backward(self, gy):
+        return gy, gy
+   
+def add(x0, x1):
+    return Add()(x0, x1)
+
+
+import contextlib
+@contextlib.contextmanager
+def using_config(name, value):
+    old_value = getattr(Config, name)
+    setattr(Config, name, value)
+    try:
+        yield
+    finally:
+        setattr(Config, name, old_value)
+    
+def no_grad():
+    return using_config('enable_backprop', False)
+
+with no_grad():
+    x = Variable(np.array(2.0))
+    y = square(x)
