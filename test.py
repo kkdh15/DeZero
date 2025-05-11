fe0b6e9d57033618911abb2f232fac4370813ec6
김도현 <kkdh15@dohyunki-MacBookAir.local>
2025-05-12 00:40:03 +0900

    step28 함수 최적화

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 import numpy as np
 from dezero import Variable
+from dezero.utils import _dot_func
 
-x = Variable(np.array(1.0))
-print(x)
+x0 = Variable(np.array(1.0))
+x1 = Variable(np.array(1.0))
+y = x0 + x1
+txt = _dot_func(y.creator)
+print(y.creator)
+print(txt)
