c862cd80050065650097040ae07a8adbd440c336
김도현 <kkdh15@dohyunki-MacBookAir.local>
2025-05-24 14:54:34 +0900

    step41 행렬의 곱

--- dezero/functions.py
+++ dezero/functions.py
@@ -124,3 +124,17 @@ class Transpose(Function):
         return gx
 def transpose(x):
     return Transpose()(x)
+
+class MatMul(Function):
+    def forward(self, x, W):
+        y = x.dot(W)
+        return y
+    
+    def backward(self, gy):
+        x, W = self.inputs
+        gx = matmul(gy, W.T)
+        gW = matmul(x.T, gy)
+        return gx, gW
+    
+def matmul(x, W):
+    return MatMul()(x, W)
